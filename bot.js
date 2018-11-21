95a7fc1b67b10a9ac077c209e8a993f33203c726
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-21 19:34:26 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+const fs = require('fs')
+
+const client = new Discord.Client();
+
+const prefix = '';
+
+client.on('ready', () => {
+
+
+ 
+ console.log(`Logged in as [ Pharaohs]`);
+
+});
+
+
+
+
+
+
+const adminprefix = "البيرفكس";
+const devs = ['447830993978523648'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+if (message.content.startsWith(adminprefix + 'ju')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk")
+    message.channel.sendMessage(`**:white_check_mark: تم تغير الحاله الي  : ${argresult}**`)
+}
+});
+
+
+
+
+
+
+
